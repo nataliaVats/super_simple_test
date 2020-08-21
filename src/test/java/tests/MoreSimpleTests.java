d1dc79000c3df509c3b791317e4f15fd0a029113
nvatstx <natalia.vats@gmail.com>
2020-08-20 22:40:13 -0500

    Init commit

---
+++ src/test/java/tests/MoreSimpleTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("more_simple_tests")
+public class MoreSimpleTests {
+
+    @Test
+    void superPositiveTest() {
+        assertEquals(3, 3);
+    }
+
+    @Tag ("not_you")
+    @Test
+    void superNegativeTest() {
+        assertEquals(2, 3);
+    }
+}
