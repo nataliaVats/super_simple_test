4c2f9cab4863d143b4632e755b27407dca24a77b
nataliaVats <69820321+nataliaVats@users.noreply.github.com>
2020-08-24 23:41:53 -0500

    Update MoreSimpleTests.java

--- src/test/java/tests/MoreSimpleTests.java
+++ src/test/java/tests/MoreSimpleTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Tag("simple_tests")
+@Tag("more_simple_tests")
 public class MoreSimpleTests {
 
     @Test
